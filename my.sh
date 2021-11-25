cc852c2f96aa66a5c3ddbd70b514763915a393e4
Sarthak Mittal <200050129@iitb.ac.in>
2021-11-25 23:06:57 +0530

    New script for crib

--- my.sh
+++ my.sh
@@ -2,7 +2,8 @@
 for i in {1..10}
 do
 rm *.txt
-java bobby.RandomPlayer 5000 > tr1.txt & java bobby.RandomPlayer 5000 > tr2.txt & java bobby.RandomPlayer 5000 > tr3.txt & java bobby.RandomPlayer 5000 > tr4.txt & java bobby.RandomPlayer 5000 > tr5.txt & java bobby.RandomPlayer 5000 > tr6.txt & sleep 2
+java bobby.RandomPlayer 5000 > tr1.txt & java bobby.RandomPlayer 5000 > tr2.txt & java bobby.RandomPlayer 5000 > tr3.txt & java bobby.RandomPlayer 5000 > tr4.txt & java bobby.RandomPlayer 5000 > tr5.txt & java bobby.RandomPlayer 5000 > tr6.txt & sleep 5
+cat tr*.txt
 python3 autograder.py -t tr* 
-echo We are at $i
+echo --------------------------------- We are at $i ---------------------------------
 done
